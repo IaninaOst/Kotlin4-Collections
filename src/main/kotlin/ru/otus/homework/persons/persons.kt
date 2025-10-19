3858c212ec3f8f64121f2c12aeee985ee816be5e
user <ianinaostashchenko@gmail.com>
2025-10-19 15:48:22 +0300

    HW Collections Task1 sortByAge, sortByName function implementation

--- src/main/kotlin/ru/otus/homework/persons/persons.kt
+++ src/main/kotlin/ru/otus/homework/persons/persons.kt
@@ -3,11 +3,14 @@ package ru.otus.homework.persons
 /**
  * Отсортировать список персон по возрасту в порядке убывания
  */
-fun List<Person>.sortByAge(): List<Person> = TODO("Доделать sortByAge")
-
+fun List<Person>.sortByAge(): List<Person> {
+    return this.sortedByDescending() { it.age }
+}
 /**
  * Отсортировать список персон по фамилии
  * - Фамилии сортируются по алфавиту в порядке возрастания
  * - Если фамилии совпадают, персоны сортируются по имени в порядке возрастания
  */
-fun List<Person>.sortByName(): List<Person> = TODO("Доделать sortBySurname")
+fun List<Person>.sortByName(): List<Person> {
+    return this.sortedWith(compareBy<Person>{ it.surname }.thenBy { it.name })
+}
